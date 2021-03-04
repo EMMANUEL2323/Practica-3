2e54bb548b90930f2ea37af67c8ce06a9ebc9eb5
EMMANUEL <curry160616@gmail.com>
2021-03-04 15:03:43 -0600

    SanPedroMartirUML.di, CuerpoCeleste, Ubicacion, MyApp, MyApp2

---
+++ SanPedroMártir4Java/src/app/ito/poo/MyApp.java
@@ -0,0 +1,18 @@
+package app.ito.poo;
+
+import clases.ito.poo.CuerpoCeleste;
+
+
+public class MyApp {
+
+	static void run() {
+		System.out.println(new CuerpoCeleste("Mercurio","435345345 y 53456234","gas"));
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		run();
+		
+	}
+
+}
---
+++ SanPedroMártir4Java/src/app/ito/poo/MyApp2.java
@@ -0,0 +1,17 @@
+package app.ito.poo;
+
+import clases.ito.poo.Ubicacion;
+
+public class MyApp2 {
+
+	static void run() {
+		System.out.println(new Ubicacion(24352,4654643,"12 meses",3252424));
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		run();
+		
+	}
+
+}
