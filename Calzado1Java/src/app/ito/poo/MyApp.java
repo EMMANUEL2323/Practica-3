5335df288afeef89c435109bff047a1fdb2b7af6
EMMANUEL <curry160616@gmail.com>
2021-03-04 04:16:37 -0600

    CalzadoUML.di, CalzadoJava, MyApp

---
+++ Calzado1Java/src/app/ito/poo/MyApp.java
@@ -0,0 +1,20 @@
+package app.ito.poo;
+
+import clases.ito.poo.Calzado;
+
+public class MyApp {
+
+
+	static void run() {
+		System.out.println(new Calzado(89393,"Sintetico","Estampado",15000,"Negro"));
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		run();
+		
+
+
+	}
+
+}
